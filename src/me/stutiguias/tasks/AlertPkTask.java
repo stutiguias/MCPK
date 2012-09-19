b9cb97fa106f7b5b27c3b2b5990bbe074454c80e
Stutiguias <dracondf@gmail.com>
2012-09-18 23:00:17 -0300

    fix error on task

--- src/me/stutiguias/tasks/AlertPkTask.java
+++ src/me/stutiguias/tasks/AlertPkTask.java
@@ -22,32 +22,39 @@ public class AlertPkTask implements Runnable {
     
     @Override
     public void run() {
-        
-        Player[] playerList = plugin.getServer().getOnlinePlayers();
-        for(String key : plugin.IsPk.keySet())
-        {         
-            if(plugin.getServer().getOnlinePlayers().length > 0)
-            {
-            Double PkPlayerX = plugin.getServer().getPlayer(key).getLocation().getX();
-            Double PkPlayerZ = plugin.getServer().getPlayer(key).getLocation().getZ();
-            for (Player player : playerList) {
-                        Double playerX = player.getLocation().getX();
-                        Double playerZ = player.getLocation().getZ();
-                        if ((playerX < PkPlayerX + (double)plugin.radius) &&
-                            (playerX > PkPlayerX - (double)plugin.radius) &&
-                            (playerZ < PkPlayerZ + (double)plugin.radius) &&
-                            (playerZ > PkPlayerZ - (double)plugin.radius) &&
-                            (player.getName().equals(key) == false) 
-                        ) {
-                            player.sendMessage(parseColor(plugin.msg.replace("%player%", key)));
+      try{
+            Player[] playerList = plugin.getServer().getOnlinePlayers();
+            for(String key : plugin.IsPk.keySet())
+            {         
+                if(plugin.getServer().getOnlinePlayers().length > 0)
+                {
+                    Player pkPlayer =  plugin.getServer().getPlayer(key);
+                    if(pkPlayer != null) {
+                        Double PkPlayerX = plugin.getServer().getPlayer(key).getLocation().getX();
+                        Double PkPlayerZ = plugin.getServer().getPlayer(key).getLocation().getZ();
+                        for (Player player : playerList) {
+                                Double playerX = player.getLocation().getX();
+                                Double playerZ = player.getLocation().getZ();
+                                if ((playerX < PkPlayerX + (double)plugin.radius) &&
+                                    (playerX > PkPlayerX - (double)plugin.radius) &&
+                                    (playerZ < PkPlayerZ + (double)plugin.radius) &&
+                                    (playerZ > PkPlayerZ - (double)plugin.radius) &&
+                                    (player.getName().equals(key) == false) 
+                                ) {
+                                    player.sendMessage(parseColor(plugin.msg.replace("%player%", key)));
+                                }
                         }
+                    }
                 }
+
+                if(plugin.getCurrentMilli() > plugin.IsPk.get(key).getTime())
+                    plugin.IsPk.remove(key);
+
             }
-            
-            if(plugin.getCurrentMilli() > plugin.IsPk.get(key).getTime())
-                plugin.IsPk.remove(key);
-                
-        }
+      } catch (Exception ex) {
+         
+      }        
+        
         
     }
     
